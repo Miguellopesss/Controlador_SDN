ca000572f1bca5e17c1ea4dcbe561bb452b22e18
Miguellopesss <2222397@my.ipleiria.pt>
2025-03-30 19:03:43 +0100

    login

---
+++ LTI_Mikrotik/LoginForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace LTI_Mikrotik
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            label1 = new Label();
+            User = new TextBox();
+            Password = new TextBox();
+            Login = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 23);
+            label1.TabIndex = 2;
+            // 
+            // User
+            // 
+            User.Location = new Point(318, 226);
+            User.Name = "User";
+            User.Size = new Size(125, 27);
+            User.TabIndex = 1;
+            // 
+            // Password
+            // 
+            Password.Location = new Point(318, 272);
+            Password.Name = "Password";
+            Password.Size = new Size(125, 27);
+            Password.TabIndex = 3;
+            // 
+            // Login
+            // 
+            Login.BackColor = SystemColors.MenuHighlight;
+            Login.ForeColor = SystemColors.ActiveCaptionText;
+            Login.Location = new Point(476, 317);
+            Login.Name = "Login";
+            Login.Size = new Size(94, 29);
+            Login.TabIndex = 4;
+            Login.Text = "Login";
+            Login.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.ErrorImage = null;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.InitialImage = null;
+            pictureBox1.Location = new Point(263, 27);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(227, 180);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(Login);
+            Controls.Add(Password);
+            Controls.Add(User);
+            Controls.Add(label1);
+            Name = "LoginForm";
+            Text = "LoginForm";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox User;
+        private TextBox Password;
+        private Button Login;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ LTI_Mikrotik/LoginForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LTI_Mikrotik
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+        private async void Login_Click(object sender, EventArgs e)
+        {
+            var urlLogin = "https://10.0.0.254/rest/interface";
+            string username = User.Text;
+            string password = Password.Text;
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                // Ignorar a validação do certificado SSL
+                ServicePointManager.ServerCertificateValidationCallback += (sender2, cert, chain, sslPolicyErrors) => true;
+
+                var byteArray = Encoding.ASCII.GetBytes($"{username}:{password}");
+                httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+
+                HttpResponseMessage response = await httpClient.GetAsync(urlLogin);
+                System.Diagnostics.Debug.WriteLine(response.StatusCode);
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    Environment.SetEnvironmentVariable("username", username);
+                    Environment.SetEnvironmentVariable("password", password);
+
+                    Form1 form1 = new Form1();
+                    form1.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Login Failed");
+                }
+            }
+        }
+
+        }
+}
